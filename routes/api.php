755e5499f3d3822d65468734a5f405ba0ebfcc48
Lanz Ruz <Lanzruizdesigns@gmail.com>
2019-08-20 02:29:40 +0800

    setting up end point

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Student;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::post('register', function(Request $request) {
+    return Student::create($request->all);
 });
